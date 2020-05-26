5269aecf21fb8f64b5518a4ca0eefd6e290b496e
Incep-3 <1425688092@qq.com>
2020-05-26 23:30:10 +0800

    -

---
+++ 树的遍历/总结/填充每个结点的下个右侧结点/s.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+    if (!root)
+        return root;
+
+    deal(root.left, root.right);
+    return root;
+};
+
+function deal(left, right) {
+    if (!left)
+        return;
+
+    left.next = right;
+    deal(left.left, left.right);
+    deal(left.right, right.left);
+    deal(right.left, right.right);
+}
